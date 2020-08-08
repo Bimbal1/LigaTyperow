0315e449343509e203c7eb17d7577fb4db0f539f
Bimbal1 <m471aaaa@gmail.com>
2020-08-08 11:02:05 +0200

    init commit

---
+++ backend/ligatyperow/src/main/java/com/example/ligatyperow/LigatyperowApplication.java
@@ -0,0 +1,13 @@
+package com.example.ligatyperow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigatyperowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigatyperowApplication.class, args);
+	}
+
+}
---
+++ backend/ligatyperow/src/test/java/com/example/ligatyperow/LigatyperowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ligatyperow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigatyperowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
