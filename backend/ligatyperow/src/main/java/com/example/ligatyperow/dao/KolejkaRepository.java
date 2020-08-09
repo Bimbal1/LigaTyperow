ff5b4a4327f705aa70c640ac844a70dd556ff294
Bimbal1 <m471aaaa@gmail.com>
2020-08-09 17:50:12 +0200

    created JpaRepositories for all entities

---
+++ backend/ligatyperow/src/main/java/com/example/ligatyperow/dao/KolejkaRepository.java
@@ -0,0 +1,7 @@
+package com.example.ligatyperow.dao;
+
+import com.example.ligatyperow.entity.Kolejka;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KolejkaRepository extends JpaRepository<Kolejka, Long> {
+}
---
+++ backend/ligatyperow/src/main/java/com/example/ligatyperow/dao/TypyRepository.java
@@ -0,0 +1,7 @@
+package com.example.ligatyperow.dao;
+
+import com.example.ligatyperow.entity.Typy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypyRepository extends JpaRepository<Typy, Long> {
+}
